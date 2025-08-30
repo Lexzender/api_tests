b67eab2e8528efc2d2ea24cf2b923143ef11ba9d
Aleksei Kostromin <kostrominpav@gmail.com>
2025-08-30 21:56:16 +0400

    Добафил фикстуры

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,16 +1,20 @@
 from http import HTTPStatus
 import pytest
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+
+from clients.authentication.authentication_client import AuthenticationClient
+from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.public_users_client import  PublicUsersClient
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_user, assert_get_user_response
+
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
-    # Инициализируем API-клиент для работы с пользователями
-    public_users_client = get_public_users_client()
+def test_create_user(public_users_client: PublicUsersClient):
 
     # Формируем тело запроса на создание пользователя
     request = CreateUserRequestSchema()
@@ -26,3 +30,17 @@ def test_create_user():
     assert_create_user_response(request, response_data)
     # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
     validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+def test_get_user_me(function_user: UserFixture,  # Используем фикстуру для создания пользователя
+        authentication_client: AuthenticationClient,
+                     private_users_client: PrivateUsersClient):
+    # Запрос на логин (login_request -> request)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    # Выполняем логин (login_response -> response)
+    authentication_client.login_api(request)
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    validate_json_schema(response.json(), response_data.model_json_schema())
+    assert_get_user_response(response_data, function_user.response)
