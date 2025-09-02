87fef6d69ae5e2b327611e05247d2a06eb029129
Aleksei Kostromin <kostrominpav@gmail.com>
2025-09-02 09:50:49 +0400

    Добавил негативные проверки

---
+++ tests/files/test_files.py
@@ -0,0 +1,58 @@
+from http import HTTPStatus
+
+import pytest
+
+from clients.errors_schema import ValidationErrorResponseSchema
+from clients.files.files_client import FilesClient
+from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
+from tools.assertions.base import assert_status_code
+from tools.assertions.files import assert_create_file_response, assert_create_file_with_empty_filename_response, \
+    assert_create_file_with_empty_directory_response
+from tools.assertions.schema import validate_json_schema
+
+
+@pytest.mark.files
+@pytest.mark.regression
+class TestFiles:
+    def test_create_file(self, files_client: FilesClient):
+        request = CreateFileRequestSchema(upload_file="./testdata/files/image.jpg")
+        response = files_client.create_file_api(request)
+        response_data = CreateFileResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_file_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+        print("Запустилс тест")
+
+    def test_create_file_with_empty_filename(self, files_client: FilesClient):
+        request = CreateFileRequestSchema(
+            filename="",
+            upload_file="./testdata/files/image.jpg"
+        )
+        response = files_client.create_file_api(request)
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+
+        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
+        assert_create_file_with_empty_filename_response(response_data)
+
+        # Дополнительная проверка структуры JSON, чтобы убедиться, что схема валидационного ответа не изменилась
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_create_file_with_empty_directory(self, files_client: FilesClient):
+        request = CreateFileRequestSchema(
+            directory="",
+            upload_file="./testdata/files/image.jpg"
+        )
+        response = files_client.create_file_api(request)
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+
+        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
+        assert_create_file_with_empty_directory_response(response_data)
+
+        # Дополнительная проверка структуры JSON
+        validate_json_schema(response.json(), response_data.model_json_schema())
---
+++ tools/assertions/files.py
@@ -0,0 +1,75 @@
+import httpx
+
+from clients.errors_schema import ValidationErrorResponseSchema, ValidationErrorSchema
+from clients.files.files_schema import CreateFileResponseSchema, CreateFileRequestSchema
+from tools.assertions.base import assert_equal
+from tools.assertions.errors import assert_validation_error_response
+
+
+def assert_create_file_response(request: CreateFileRequestSchema, response: CreateFileResponseSchema):
+    """
+    Проверяет, что ответ на создание файла соответствует запросу.
+
+    :param request: Исходный запрос на создание файла.
+    :param response: Ответ API с данными файла.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    # Формируем ожидаемую ссылку на загруженный файл
+    expected_url = f"http://localhost:8000/static/{request.directory}/{request.filename}"
+
+    assert_equal(str(response.file.url), expected_url, "url")
+    assert_equal(response.file.filename, request.filename, "filename")
+    assert_equal(response.file.directory, request.directory, "directory")
+
+
+def assert_file_is_accessible(url: str):
+    """
+    Проверяет, что файл доступен по указанному URL.
+
+    :param url: Ссылка на файл.
+    :raises AssertionError: Если файл не доступен.
+    """
+    response = httpx.get(url)
+    assert response.status_code == 200, f"Файл недоступен по URL: {url}"
+
+
+def assert_create_file_with_empty_filename_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на создание файла с пустым именем файла соответствует ожидаемой валидационной ошибке.
+
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[
+            ValidationErrorSchema(
+                type="string_too_short",
+                input="",
+                context={"min_length": 1},
+                message="String should have at least 1 character",
+                location=["body", "filename"]
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
+
+
+def assert_create_file_with_empty_directory_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на создание файла с пустым значением директории соответствует ожидаемой валидационной ошибке.
+
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[
+            ValidationErrorSchema(
+                type="string_too_short",
+                input="",
+                context={"min_length": 1},
+                message="String should have at least 1 character",
+                location=["body", "directory"]
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
